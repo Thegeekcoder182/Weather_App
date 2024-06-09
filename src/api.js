73480818da69a09f2804306f17cfcee7b40a97a2
Thegeekcoder182 <ayanbarai185@gmail.com>
2024-06-09 21:10:26 +0530

    Created a Weather App using React

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
+    "X-RapidAPI-Host": process.env.X_RAPIDAPI_HOST,
   },
 };
+
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
